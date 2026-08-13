3c70dcdada628e109f91f87d0d1967fb275aca62
mariaecandido6-lgtm <maria.e.candido6@edu.senai.br>
2026-08-13 11:29:50 -0300

    ex.17

---
+++ ex17.js
@@ -0,0 +1,10 @@
+function calcularSalario(salario, bonus) {
+    let valorBonus = salario * bonus  /100;
+    let salarioFinal = salario + valorBonus
+
+    return salarioFinal
+
+}
+
+let salario = calcularSalario(3000,10)
+console.log(`salário final: R$ ${salario}`);
